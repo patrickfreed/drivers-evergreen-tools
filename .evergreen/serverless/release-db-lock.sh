25399cc95b0661c0b06e630f4d586ec0ce40e077
Patrick Freed <patrick.freed@mongodb.com>
2021-03-18 14:40:09 -0400

    delete old serverless junk

--- .evergreen/serverless/release-db-lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-${MONGODB_BINARIES}/mongo \
-                   --eval "const mongodbUri = '${MONGODB_URI}';\
-                           const taskId = '${task_id}'" \
-                   release-db-lock.js
